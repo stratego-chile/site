26e722644b73176f49aaec35df3a1c61e963682a
KBeDevel <theuserbit@proton.me>
2022-11-30 17:39:07 -0300

    feat: add static data

---
+++ src/data/home-content.ts
@@ -0,0 +1,68 @@
+import {
+  faBug,
+  faCloudBolt,
+  faFileShield,
+  faGraduationCap,
+  faLifeRing,
+  faShieldHalved,
+  faSignsPost,
+  faUserSecret,
+  faUserShield,
+} from '@fortawesome/free-solid-svg-icons'
+
+export const advantages = [
+  {
+    icon: faShieldHalved,
+    title: 'sections:home.fragments.advantages.items.privacy.title',
+    description: 'sections:home.fragments.advantages.items.privacy.description',
+  },
+  {
+    icon: faLifeRing,
+    title: 'sections:home.fragments.advantages.items.support.title',
+    description: 'sections:home.fragments.advantages.items.support.description',
+  },
+  {
+    icon: faGraduationCap,
+    title: 'sections:home.fragments.advantages.items.training.title',
+    description:
+      'sections:home.fragments.advantages.items.training.description',
+  },
+]
+
+export const services = [
+  {
+    icon: faBug,
+    title: 'sections:home.fragments.services.items.malwareSecurity.title',
+    description:
+      'sections:home.fragments.services.items.malwareSecurity.description',
+  },
+  {
+    icon: faFileShield,
+    title: 'sections:home.fragments.services.items.dataSecurity.title',
+    description:
+      'sections:home.fragments.services.items.dataSecurity.description',
+  },
+  {
+    icon: faCloudBolt,
+    title: 'sections:home.fragments.services.items.cloudSecurity.title',
+    description:
+      'sections:home.fragments.services.items.cloudSecurity.description',
+  },
+  {
+    icon: faUserSecret,
+    title: 'sections:home.fragments.services.items.webSecurity.title',
+    description:
+      'sections:home.fragments.services.items.webSecurity.description',
+  },
+  {
+    icon: faUserShield,
+    title: 'sections:home.fragments.services.items.databaseSecurity.title',
+    description:
+      'sections:home.fragments.services.items.databaseSecurity.description',
+  },
+  {
+    icon: faSignsPost,
+    title: 'sections:home.fragments.services.items.planning.title',
+    description: 'sections:home.fragments.services.items.planning.description',
+  },
+]
