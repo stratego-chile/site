837a00a6652bd9d7da89712c2a380f3164d6f54c
KBeDevel <theuserbit@proton.me>
2023-01-16 11:43:24 -0300

    feat: centralize section links definitions

---
+++ src/data/navigation-links.ts
@@ -0,0 +1,48 @@
+export type LinkSpec = {
+  text: string
+  label?: boolean
+  href?: string
+  subLinks?: Array<LinkSpec>
+  disabled?: boolean
+}
+
+export const cybersecurityLinks: Array<LinkSpec> = [
+  {
+    text: 'sections:security.services.audit.title',
+    subLinks: [
+      {
+        href: '/services/cybersecurity/audit?subsection=risks-and-vulnerabilities',
+        text: 'sections:security.services.audit.modules.risksAndVulnerabilities.title',
+      },
+      {
+        href: '/services/cybersecurity/audit?subsection=infrastructure',
+        text: 'sections:security.services.audit.modules.infrastructure.title',
+      },
+      {
+        href: '/services/cybersecurity/audit?subsection=procedures',
+        text: 'sections:security.services.audit.modules.procedures.title',
+      },
+      {
+        href: '/services/cybersecurity/audit?subsection=mitigation',
+        text: 'sections:security.services.audit.modules.mitigation.title',
+      },
+      {
+        href: '/services/cybersecurity/audit?subsection=social-engineering',
+        text: 'sections:security.services.audit.modules.socialEngineering.title',
+      },
+    ],
+  },
+  {
+    text: 'sections:security.services.consulting.title',
+    subLinks: [
+      {
+        href: '/services/cybersecurity/consulting?subsection=ISO-IEC-27000',
+        text: 'sections:security.services.consulting.modules.iso27000.title',
+      },
+      {
+        href: '/services/cybersecurity/consulting?subsection=CHL-21459',
+        text: 'sections:security.services.consulting.modules.chl21459.title',
+      },
+    ],
+  },
+]
