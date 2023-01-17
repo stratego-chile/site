3929106303872987948b6d35220e34d719153677
KBeDevel <theuserbit@proton.me>
2023-01-17 12:25:43 -0300

    feat: Next dynamic loading placeholder

---
+++ src/components/utils/loading-placeholder.tsx
@@ -0,0 +1,23 @@
+import { type FC } from 'react'
+import Alert from 'react-bootstrap/Alert'
+import Spinner from 'react-bootstrap/Spinner'
+
+type LoadingPlaceholderProps = {
+  loading?: boolean
+  error?: Error | null
+}
+
+const LoadingPlaceholder: FC<LoadingPlaceholderProps> = ({
+  loading = true,
+  error,
+}) => {
+  return loading ? (
+    <div style={{ height: 'inherit', width: 'inherit' }}>
+      <Spinner />
+    </div>
+  ) : error ? (
+    <Alert variant="danger">{error.toString()}</Alert>
+  ) : null
+}
+
+export default LoadingPlaceholder
