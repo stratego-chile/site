097c7c560cad07695e8296464c7523a06fce8055
KBeDevel <theuserbit@proton.me>
2023-01-23 12:25:38 -0300

    fix: add missing type to `localesList` in middleware

--- src/locales.ts
+++ src/locales.ts
@@ -1,5 +1,5 @@
 import { i18n } from '@stratego/../next-i18next.config'
 
-export const localesList = i18n.locales
+export const localesList = i18n.locales as Array<AvailableLocales>
 
 export const { defaultLocale } = i18n
