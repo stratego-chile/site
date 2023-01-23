25b10671cfac08dd328eaee20ec12e17d823e4f4
KBeDevel <theuserbit@proton.me>
2023-01-23 12:27:26 -0300

    feat: create captcha API reusable module & optimize contact form receptor

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,36 +1,14 @@
-import template from '@stratego/mail/templates/contact.pug'
+import defaultTranslation from '@stratego/mail/i18n/es-CL.json'
+import template from '@stratego/templates/contact.pug'
 import type { NextApiHandler } from 'next'
 import BootstrapStyles from '!!raw-loader!bootstrap/dist/css/bootstrap.min.css'
 import mailer, { type SendMailOptions } from 'nodemailer'
-import { i18n } from '@stratego/../next-i18next.config'
 import { type LocalsObject } from 'pug'
 import format from '@stdlib/string/format'
-import axios from 'axios'
+import { checkCaptchaToken } from '@stratego/pages/api/(captcha)'
+import { defaultLocale, localesList } from '@stratego/locales'
 
 const ALLOWED_METHODS = ['OPTIONS', 'POST']
-
-const checkCaptchaToken = async (token: string) => {
-  const destinationURL = new URL(process.env.CAPTCHA_VERIFIER_API)
-
-  destinationURL.searchParams.append('secret', process.env.CAPTCHA_SECRET)
-  destinationURL.searchParams.append('response', token)
-
-  const { data } = await axios.post<{
-    success: boolean
-    score: number
-    action: string
-    challenge_ts: string
-    hostname: string
-    'error-codes'?: string[]
-  }>(destinationURL.toString(), undefined, {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Accept: 'application/json; charset=utf-8',
-    },
-  })
-
-  return data.success && data.score > parseFloat(process.env.CAPTCHA_MIN_SCORE)
-}
 
 const handler: NextApiHandler<ResponseBody<undefined>> = async (
   request,
@@ -38,123 +16,116 @@ const handler: NextApiHandler<ResponseBody<undefined>> = async (
 ) => {
   const captchaToken = request.headers.authorization
 
-  if (!ALLOWED_METHODS.includes(request.method!)) {
+  if (!ALLOWED_METHODS.includes(String(request.method).toUpperCase())) {
     response
       .status(405)
       .json({ status: 'ERROR', message: 'Method not allowed' })
+  } else if (request.method === 'OPTIONS') {
+    response
+      .status(200)
+      .setHeader('Allow', ALLOWED_METHODS.slice(1).join(', '))
+      .end()
+  } else if (!captchaToken || !(await checkCaptchaToken(captchaToken))) {
+    response
+      .status(403)
+      .json({ status: 'ERROR', message: 'Captcha token invalid' })
   } else {
-    if (request.method === 'OPTIONS') {
-      response.status(200).end()
-    } else if (!captchaToken || !(await checkCaptchaToken(captchaToken))) {
-      response
-        .status(403)
-        .json({ status: 'ERROR', message: 'Captcha token invalid' })
-    } else {
-      try {
-        const providedLocale =
-          'accept-language' in request.headers &&
-          request.headers['accept-language']!
+    try {
+      const providedLocale =
+        'accept-language' in request.headers &&
+        (request.headers['accept-language']! as AvailableLocales)
 
-        const locale: AvailableLocales =
-          providedLocale &&
-          providedLocale in (i18n.locales as Array<AvailableLocales>)
-            ? (providedLocale as AvailableLocales)
-            : (i18n.defaultLocale as AvailableLocales)
+      const locale =
+        providedLocale && providedLocale in localesList
+          ? providedLocale
+          : (defaultLocale as AvailableLocales)
 
-        const translation = (
-          await (async () => {
-            switch (locale) {
-              case 'en-US':
-                return await import('@stratego/mail/i18n/en-US.json')
-              case 'pt-BR':
-                return await import('@stratego/mail/i18n/pt-BR.json')
-              default:
-                return await import('@stratego/mail/i18n/es-CL.json')
-            }
-          })()
-        ).default
+      // Prevents the unnecessary import of unused locales
+      const translation: typeof defaultTranslation =
+        locale === (defaultLocale as AvailableLocales)
+          ? defaultTranslation
+          : (await import(`@stratego/mail/i18n/${locale}.json`)).default
 
-        const {
-          businessName,
-          email,
-          message,
-          name,
-          phoneNumber,
-          phonePrefix,
-          surname,
-        } = request.body as Partial<{
-          name: string
-          surname: string
-          phonePrefix: string
-          phoneNumber: string
-          businessName: string
-          email: string
-          message: string
-        }>
+      const {
+        businessName,
+        email,
+        message,
+        name,
+        phoneNumber,
+        phonePrefix,
+        surname,
+      } = request.body as Partial<{
+        name: string
+        surname: string
+        phonePrefix: string
+        phoneNumber: string
+        businessName: string
+        email: string
+        message: string
+      }>
 
-        const locals: LocalsObject = {
-          locale,
-          cssStyles: BootstrapStyles,
-          title: translation.title,
-          message: {
-            title: format(translation.subTitle, name, surname),
-            contactData: [
-              format(translation.orgName, businessName),
-              format(translation.email, email),
-              format(translation.phoneNumber, phonePrefix, phoneNumber),
-            ],
-            content: [translation.message, message],
-            footer: translation.footer,
-          },
-        }
-
-        const compiledTemplate = template(locals)
-
-        const mailTransporter = mailer.createTransport({
-          host: process.env.MAILER_HOST,
-          port: parseInt(process.env.MAILER_PORT),
-          secure: true,
-          auth: {
-            type: process.env.MAILER_TYPE,
-            user: process.env.MAILER_USER,
-            pass: process.env.MAILER_PASS,
-          },
-        })
-
-        const mailOptions: SendMailOptions = {
-          from: process.env.MAILER_FROM,
-          to: process.env.MAILER_TO,
-          encoding: 'utf-8',
-          subject: locals.title,
-          text: `
-            # ${locals.message.title}
-
-            ${locals.message.contactData[0]}
-
-            ${locals.message.contactData[1]}
-
-            ${locals.message.contactData[2]}
-
-            ${locals.message.content[0]}
-            ${locals.message.content[1]}
-
-            ${locals.message.footer}
-          `,
-          html: compiledTemplate,
-        }
-
-        await mailTransporter.sendMail(mailOptions)
-
-        response.status(200).json({
-          status: 'OK',
-        })
-      } catch (error) {
-        response.status(500).json({
-          status: 'ERROR',
-          message: 'Unexpected error',
-          trace: error || undefined,
-        })
+      const locals: LocalsObject = {
+        locale,
+        cssStyles: BootstrapStyles,
+        title: translation.title,
+        message: {
+          title: format(translation.subTitle, name, surname),
+          contactData: [
+            format(translation.orgName, businessName),
+            format(translation.email, email),
+            format(translation.phoneNumber, phonePrefix, phoneNumber),
+          ],
+          content: [translation.message, message],
+          footer: translation.footer,
+        },
       }
+
+      const compiledTemplate = template(locals)
+
+      const mailTransporter = mailer.createTransport({
+        host: process.env.MAILER_HOST,
+        port: parseInt(process.env.MAILER_PORT),
+        secure: true,
+        auth: {
+          type: process.env.MAILER_TYPE,
+          user: process.env.MAILER_USER,
+          pass: process.env.MAILER_PASS,
+        },
+      })
+
+      const mailOptions: SendMailOptions = {
+        from: process.env.MAILER_FROM,
+        to: process.env.MAILER_TO,
+        encoding: 'utf-8',
+        subject: locals.title,
+        text: `
+          # ${locals.message.title}
+
+          ${locals.message.contactData[0]}
+
+          ${locals.message.contactData[1]}
+
+          ${locals.message.contactData[2]}
+
+          ${locals.message.content[0]}
+          ${locals.message.content[1]}
+
+          ${locals.message.footer}
+        `,
+        html: compiledTemplate,
+      }
+
+      await mailTransporter.sendMail(mailOptions)
+
+      response.status(200).json({
+        status: 'OK',
+      })
+    } catch (error) {
+      response.status(500).json({
+        status: 'ERROR',
+        message: 'Unexpected error',
+        trace: error || undefined,
+      })
     }
   }
 }
