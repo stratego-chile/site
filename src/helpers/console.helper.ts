3ccf55d22a2210a7677652d12c017f5006af3980
KBeDevel <theuserbit@proton.me>
2023-01-23 17:57:39 -0300

    fix: typo in console helper

--- src/helpers/console.helper.ts
+++ src/helpers/console.helper.ts
@@ -5,11 +5,11 @@ export const showConsoleWarnings = (): void => {
         '%cHOLD UP! This tool is supposed to be used by advanced users',
         'Any action executed here could expose your data and/or generate unexpected application behavior.',
       ].join('\n'),
-      'background: blue; color: white; font-size: x-large; font-weight: bold;',
+      'background: #0079E5; color: white; font-size: x-large; font-weight: bold;',
     ],
     [
       [
-        '%cIf you want to make a report or provide us a feedback, just send an email to dev@stratego.cl',
+        '%cIf you want to make a report or provide us feedback, just send an email to dev@stratego.cl',
         'or file an issue on https://github.com/stratego-chile/site',
       ].join(' '),
       'color: whitesmoke; font-size: large; font-weight: light;',
