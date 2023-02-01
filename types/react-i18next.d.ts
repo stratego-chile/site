3f20e6f8e9b60bfd3596a9ba6bf81aa2a2b5d55c
KBeDevel <theuserbit@proton.me>
2023-02-01 13:13:06 -0300

    fix: remove useless namespace assertion type for next-i18next config

--- types/react-i18next.d.ts
+++ types/react-i18next.d.ts
@@ -1,18 +1,8 @@
 import 'react-i18next'
 
-import type common from '../public/locales/es-CL/common.json'
-import type sections from '../public/locales/es-CL/sections.json'
-import type validation from '../public/locales/es-CL/validation.json'
-
-interface I18nNamespaces {
-  common: typeof common
-  sections: typeof sections
-  validation: typeof validation
-}
-
 declare module 'react-i18next' {
   interface CustomTypeOptions {
     defaultNS: 'common'
-    resources: I18nNamespaces
+    resources: Array<string>
   }
 }
