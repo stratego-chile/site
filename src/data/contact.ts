b8a60d16487e6379d9e2afef21a884eea62a66ca
KBeDevel <theuserbit@proton.me>
2023-02-01 13:21:29 -0300

    feat: add typing to contact data list and extend information

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,6 +1,24 @@
-import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import {
+  faEnvelope,
+  faLocationDot,
+  faPhone,
+  type IconDefinition,
+} from '@fortawesome/free-solid-svg-icons'
 
-export const contactData = [
+type ContactFragment = {
+  icon: IconDefinition
+  linkPrefix?: string
+  link?: string
+  text?: Array<string> | string
+}
+
+export const contactData: Array<ContactFragment> = [
+  {
+    icon: faLinkedin,
+    text: 'Stratego Tech',
+    link: 'https://www.linkedin.com/company/strategotech/',
+  },
   {
     icon: faEnvelope,
     text: 'contact@stratego.cl',
@@ -8,7 +26,12 @@ export const contactData = [
   },
   {
     icon: faPhone,
-    text: '+56 9 7793 4344',
     linkPrefix: 'tel',
+    text: '+56 9 7793 4344',
+  },
+  {
+    icon: faLocationDot,
+    text: ['Padre Mariano 272', 'Oficina 302', 'Providencia, Santiago, Chile'],
+    link: 'https://www.openstreetmap.org/way/579051556#map=19/-33.42477/-70.61791',
   },
 ]
