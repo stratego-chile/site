d8312503ae7e26a49bc2ed1e93f57c0a6ee32549
KBeDevel <theuserbit@proton.me>
2023-02-01 13:22:37 -0300

    feat: add endpoint wrapper to default docs API

--- src/pages/api/default-docs.ts
+++ src/pages/api/default-docs.ts
@@ -1,28 +1,20 @@
-import { NextApiHandler } from 'next'
-import { i18n } from 'next-i18next'
+import type { NextApiHandler } from 'next'
+import type { Method } from 'axios'
+import endpoint from '@stratego/pages/api/(endpoint)'
 
-const ALLOWED_METHODS = ['OPTIONS', 'GET']
+const ALLOWED_METHODS: Array<Method> = ['GET']
 
 const handler: NextApiHandler<
   Array<{ id: number | string; title: string }> | ResponseBody<undefined>
-> = async (request, response) => {
-  if (!ALLOWED_METHODS.includes(String(request.method).toUpperCase())) {
-    response
-      .status(405)
-      .json({ status: 'ERROR', message: 'Method not allowed' })
-  } else if (request.method === 'OPTIONS') {
-    response
-      .status(200)
-      .setHeader('Allow', ALLOWED_METHODS.slice(1).join(', '))
-      .end()
-  } else {
+> = async (...hooks) => {
+  endpoint(ALLOWED_METHODS, ...hooks, (_request, response) => {
     response.status(200).json([
       {
         id: 'information-security',
-        title: String(i18n?.t('docs:articles.default.informationSecurity')),
+        title: 'docs:articles.default.informationSecurity',
       },
     ])
-  }
+  })
 }
 
 export default handler
