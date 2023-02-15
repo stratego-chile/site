a6078dbe796555e53808b19ce5b26abc73c03aef
KBeDevel <theuserbit@proton.me>
2023-02-15 18:14:12 -0300

    fix: remove unused env variables

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// @ts-check
 const path = require('path')
 const { i18n } = require('./next-i18next.config')
 
@@ -44,17 +45,13 @@ const nextConfig = {
   },
   // Add environment variables to the client side
   env: {
-    BRAND_NAME: process.env.BRAND_NAME,
-    BRAND_JURIDICAL_NAME: process.env.BRAND_JURIDICAL_NAME,
-    DEFAULT_PAGE_TITLE: process.env.DEFAULT_PAGE_TITLE,
-    DEFAULT_PAGE_DESCRIPTION: process.env.DEFAULT_PAGE_DESCRIPTION,
-    DEFAULT_ASSETS_SOURCE: process.env.DEFAULT_ASSETS_SOURCE,
-    DOCS_POSTS_SOURCE: process.env.DOCS_POSTS_SOURCE,
-    DOCS_PUSHER_CHANNEL: process.env.DOCS_PUSHER_CHANNEL,
-    CAPTCHA_KEY: process.env.CAPTCHA_KEY,
-    PUSHER_APP_ID: process.env.PUSHER_APP_ID,
-    PUSHER_APP_KEY: process.env.PUSHER_APP_KEY,
-    PUSHER_APP_CLUSTER: process.env.PUSHER_APP_CLUSTER,
+    BRAND_NAME: process.env.BRAND_NAME ?? '',
+    BRAND_JURIDICAL_NAME: process.env.BRAND_JURIDICAL_NAME ?? '',
+    DEFAULT_PAGE_TITLE: process.env.DEFAULT_PAGE_TITLE ?? '',
+    DEFAULT_PAGE_DESCRIPTION: process.env.DEFAULT_PAGE_DESCRIPTION ?? '',
+    DEFAULT_ASSETS_SOURCE: process.env.DEFAULT_ASSETS_SOURCE ?? '',
+    DOCS_POSTS_SOURCE: process.env.DOCS_POSTS_SOURCE ?? '',
+    CAPTCHA_KEY: process.env.CAPTCHA_KEY ?? '',
   },
 }
 
