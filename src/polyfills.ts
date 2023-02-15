01a97aea29728bb97d40c32906ed0659b700b4a7
KBeDevel <theuserbit@proton.me>
2023-02-15 18:14:35 -0300

    fix: remove unused polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,8 +1,5 @@
-import 'core-js/features/array/group'
-import 'core-js/features/array/group-to-map'
-
-// Add `hasItems` function to Array prototype in isolated mode
 ;(() => {
+  // Add `hasItems` function to Array prototype in isolated mode
   const hasItems = 'hasItems'
   if (!Object.prototype.hasOwnProperty.call(Array, hasItems)) {
     Object.defineProperty(Array, hasItems, {
@@ -15,30 +12,8 @@ import 'core-js/features/array/group-to-map'
       return this.length > 0
     }
   }
-})()
 
-// Add `clonesOf` function to Array prototype in isolated mode
-;(() => {
-  const clonesOf = 'clonesOf'
-  if (!Object.prototype.hasOwnProperty.call(Array, clonesOf)) {
-    Object.defineProperty(Array, clonesOf, {
-      enumerable: false,
-      configurable: false,
-      writable: false,
-      value: 'static',
-    })
-    Array.prototype[clonesOf] = function (comparator) {
-      return this.every((item) =>
-        item instanceof Object
-          ? Object.is(comparator, item)
-          : item === comparator
-      )
-    }
-  }
-})()
-
-// Add `surround` function to String prototype in isolated mode
-;(() => {
+  // Add `surround` function to String prototype in isolated mode
   const surround = 'surround'
   if (!Object.prototype.hasOwnProperty.call(String, surround)) {
     Object.defineProperty(String, surround, {
@@ -52,3 +27,5 @@ import 'core-js/features/array/group-to-map'
     }
   }
 })()
+
+export {}
