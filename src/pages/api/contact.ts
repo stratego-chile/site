d7458a7c1c646031636b916bba79cffba834acd0
KBeDevel <theuserbit@proton.me>
2023-03-30 16:25:57 -0300

    break: refactor mailing locales in submodule

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,15 +1,17 @@
 import BootstrapStyles from '!!raw-loader!bootstrap/dist/css/bootstrap.min.css'
 import format from '@stdlib/string/format'
 import { defaultLocale, localesList } from '@stratego/locales'
-import defaultTranslation from '@stratego/mail/i18n/es-CL.json'
 import { checkCaptchaToken } from '@stratego/pages/api/(captcha)'
 import endpoint from '@stratego/pages/api/(endpoint)'
 import template from '@stratego/templates/contact.pug'
 import type { Method } from 'axios'
 import { StatusCodes } from 'http-status-codes'
+import i18next from 'i18next'
+import i18nextBackend from 'i18next-fs-backend'
 import type { NextApiHandler } from 'next'
 import mailer, { type SendMailOptions } from 'nodemailer'
-import { type LocalsObject } from 'pug'
+import path from 'path'
+import type { LocalsObject } from 'pug'
 
 const ALLOWED_METHODS: Array<Method> = ['POST']
 
@@ -24,20 +26,30 @@ const handler: NextApiHandler<Stratego.Common.ResponseBody<undefined>> = async (
         .status(StatusCodes.FORBIDDEN)
         .json({ status: 'ERROR', message: 'Captcha token invalid' })
 
-    const providedLocale =
-      'accept-language' in request.headers &&
-      (request.headers['accept-language']! as Stratego.Common.Locale)
+    const locale = ((providedLocale) => {
+      return (
+        providedLocale &&
+        localesList.includes(providedLocale as Stratego.Common.Locale)
+          ? providedLocale
+          : defaultLocale
+      ) as Stratego.Common.Locale
+    })(request.headers['accept-language'] as string | undefined)
 
-    const locale =
-      providedLocale && providedLocale in localesList
-        ? providedLocale
-        : (defaultLocale as Stratego.Common.Locale)
+    const getTranslation = await i18next.use(i18nextBackend).init({
+      lng: locale,
+      ns: 'mail',
+      backend: {
+        loadPath: path.join(
+          process.cwd(),
+          '/public/locales/{{lng}}/{{ns}}.json'
+        ),
+      },
+    })
 
-    // Prevents the unnecessary import of unused locales
-    const translation: typeof defaultTranslation =
-      locale === (defaultLocale as Stratego.Common.Locale)
-        ? defaultTranslation
-        : (await import(`@stratego/mail/i18n/${locale}.json`)).default
+    const translation: Record<string, string> = getTranslation('mail:contact', {
+      returnObjects: true,
+      lng: locale,
+    })
 
     const formData = request.body as Partial<{
       name: string
